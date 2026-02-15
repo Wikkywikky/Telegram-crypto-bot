301ffd547e6a77a8d2c351c9b43c96ba28187ba5
Wikky Wikky <44517289+Wikkywikky@users.noreply.github.com>
2026-02-15 08:59:14 +0700

    Add constants for transaction states in states.py

---
+++ states.py
@@ -0,0 +1,31 @@
+# =========================
+# TOPUP
+# =========================
+TOPUP_AMOUNT = "TOPUP_AMOUNT"
+TOPUP_METHOD = "TOPUP_METHOD"
+TOPUP_PROOF  = "TOPUP_PROOF"
+TOPUP_NAME   = "TOPUP_NAME"
+
+# =========================
+# BUY
+# =========================
+BUY_TOKEN   = "BUY_TOKEN"
+BUY_NETWORK = "BUY_NETWORK"
+BUY_AMOUNT  = "BUY_AMOUNT"
+BUY_WALLET  = "BUY_WALLET"
+BUY_CONFIRM = "BUY_CONFIRM"
+
+# =========================
+# SELL
+# =========================
+SELL_AMOUNT = "SELL_AMOUNT"
+SELL_SENDER = "SELL_SENDER"
+SELL_TX     = "SELL_TX"
+
+# =========================
+# WITHDRAW
+# =========================
+WD_METHOD = "WD_METHOD"
+WD_TARGET = "WD_TARGET"
+WD_NAME   = "WD_NAME"
+WD_AMOUNT = "WD_AMOUNT"
